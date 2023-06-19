d98f2e429942e67e8186671220a8728b4f3ba002
Pavel <pavelguest@yandex.ru>
2023-06-19 10:17:30 +0300

    fix: errors woth  path

--- src/App.vue
+++ src/App.vue
@@ -7,34 +7,32 @@
 </template>
 
 <script>
-import { layoutNames } from '@/utils/constants'
-import { ErrorLayout, MainLayout } from '@/layouts'
+import { layoutNames } from "@/utils/constants";
+import { ErrorLayout, MainLayout } from "@/layouts";
 
 export default {
   data: () => ({
     layouts: {
       ErrorLayout,
-      MainLayout
-    }
+      MainLayout,
+    },
   }),
 
   computed: {
-    layout () {
-      const layoutName = this.$route.meta.layout || layoutNames.MAIN_LAYOUT
+    layout() {
+      const layoutName = this.$route.meta.layout || layoutNames.MAIN_LAYOUT;
 
-      return this.layouts[layoutName]
-    }
-  }
-}
-
+      return this.layouts[layoutName];
+    },
+  },
+};
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 @import "./assets/styles/common/index";
 
 #app {
   font-size: $font-size-large;
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
-
 </style>
